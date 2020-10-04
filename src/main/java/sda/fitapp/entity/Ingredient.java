8bb7c8c7ba7cbe741f8f1cb231119f417127106c
kkobrok <kkobrok@gmail.com>
2020-10-04 09:46:09 +0200

    First model of relation of ingredients to proportion inside meal. Also higher version of lombok and remove builder and args constructor + to prevent glitches

--- src/main/java/sda/fitapp/entity/Ingredient.java
+++ src/main/java/sda/fitapp/entity/Ingredient.java
@@ -1,22 +1,16 @@
 package sda.fitapp.entity;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
-@Builder
-@NoArgsConstructor
+@Table(name = "ingredient")
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private long id;
     private String name;
     private long kcal100g;
