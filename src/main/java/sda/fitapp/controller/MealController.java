ae54f9d711926430df0f1d94e02d90beee3c9438
kkobrok <kkobrok@gmail.com>
2020-10-18 12:32:04 +0200

    endpoints for front end

--- src/main/java/sda/fitapp/controller/MealController.java
+++ src/main/java/sda/fitapp/controller/MealController.java
@@ -3,10 +3,11 @@ package sda.fitapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sda.fitapp.dto.MealRequest;
-import sda.fitapp.entity.Meal;
+import sda.fitapp.entity.*;
 import sda.fitapp.repository.MealRepositories;
 import sda.fitapp.service.MealService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -30,10 +31,86 @@ public class MealController {
     public List<Meal> getAllMeal() {
         return mealService.getAllMeal();
     }
-    @GetMapping("/api/meal/{id}")
-    public Meal getMealById(@RequestParam long id){
+
+    @GetMapping("/id/{id}")
+    public Meal getMealById(@RequestParam long id) {
         return mealService.getMealById(id);
     }
 
+    @GetMapping("/breakfast")
+    public List<Meal> getMealBreakfast() {
+        List<WrapperIngredientToProportion> wrapperIngredientToProportionList = new ArrayList<>();
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 1,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 2,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+
+        return mealList;
+    }
+
+    @GetMapping("/second_breakfast")
+    public List<Meal> getMealSecondBreakfast() {
+        List<WrapperIngredientToProportion> wrapperIngredientToProportionList = new ArrayList<>();
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 1,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 2,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+
+        return mealList;
+    }
+
+    @GetMapping("/dinner")
+    public List<Meal> getMealDinner() {
+        List<WrapperIngredientToProportion> wrapperIngredientToProportionList = new ArrayList<>();
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 1,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 2,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+
+        return mealList;
+    }
+
+    @GetMapping("/supper")
+    public List<Meal> getMealSupper() {
+        List<WrapperIngredientToProportion> wrapperIngredientToProportionList = new ArrayList<>();
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 1,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+        wrapperIngredientToProportionList.add(new WrapperIngredientToProportion((long) 2,new Ingredient(1,"a",2,1,2,1,
+                true,true,true),1.5 ));
+
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+        tagList.add(new Tag((long)1,"a", TagsType.MEAL));
+
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+        mealList.add(new Meal(1, "a","a",wrapperIngredientToProportionList, true,true,true,true,tagList));
+
+        return mealList;
+    }
 
 }
