c41eb2a548c0ecd1a2e67b00ffdda0e0cded547a
barjaskulski <barjaskulski@gmail.com>
2020-10-24 10:46:27 +0200

    #47 Added all cases for security test

--- src/test/java/sda/fitapp/SecurityConfigTest.java
+++ src/test/java/sda/fitapp/SecurityConfigTest.java
@@ -7,9 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-
-import static javax.swing.UIManager.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,10 +20,85 @@ public class SecurityConfigTest {
     @Autowired
     private MockMvc mvc;
 
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = "USER")
+    public void configure0() throws Exception {         // ----------- access only for ALL
+        this.mvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = "ADMIN")
+    public void configure1() throws Exception {         // ----------- access only for ADMIN role
+        this.mvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+//    @Test                                               //-------------- WIP - no questionare
+//    @WithMockUser(username = "user", password = "user", roles = "USER")
+//    public void configure2() throws Exception {         // ----------- access only for ALL
+//        this.mvc.perform(post("/questionnaire"))
+//                .andDo(print())
+//                .andExpect(status().is2xxSuccessful());
+//    }
+
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = "SUBSCRIBED_USER")
+    public void configure3() throws Exception {        // ----------- access only for SUBSCRIBED_USER & ADMIN role
+        this.mvc.perform(get("/api/meal"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+//                                                        //------------- WIP
 //    @Test
-//    @WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
-//    public void configure() throws Exception {
-//        this.mvc.perform(get("/users"))
+//    @WithMockUser(username = "user", password = "user", roles = "ADMIN")
+//    public void configure4() throws Exception {        // ----------- access only for ADMIN role
+//        this.mvc.perform(get("/api/meal/**"))
+//                .andDo(print())
+//                .andExpect(status().is2xxSuccessful());
+//    }
+
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = "SUBSCRIBED_USER")
+    public void configure5() throws Exception {         // ----------- access only for SUBSCRIBED_USER & ADMIN role
+        this.mvc.perform(get("/ingredient"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+//                                                        //------------- WIP
+//    @Test
+//    @WithMockUser(username = "user", password = "user", roles = "ADMIN")
+//    public void configure6() throws Exception {         // ----------- access only for ADMIN role
+//        this.mvc.perform(get("/ingredient/**"))
+//                .andDo(print())
+//                .andExpect(status().is2xxSuccessful());
+//    }
+
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = "USER")
+    public void configure7() throws Exception {         // ----------- access only for ALL role
+        this.mvc.perform(get("/swagger-ui.html"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+//                                                        //------------- WIP
+//    @Test
+//    @WithMockUser(username = "user", password = "user", roles = "USER")
+//    public void configure8() throws Exception {         // ----------- access only for ALL role
+//        this.mvc.perform(get("/swagger-ui.html/**"))
+//                .andDo(print())
+//                .andExpect(status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    @WithMockUser(username = "user", password = "user", roles = "USER")
+//    public void configure9() throws Exception {         // ----------- access only for ALL role
+//        this.mvc.perform(get("/swagger-resources.html/**"))
 //                .andDo(print())
 //                .andExpect(status().is2xxSuccessful());
 //    }
